8876c011f9956e5905a28469bba80cb740d00b84
Ivan <sloviane@mail.ru>
2025-11-11 09:16:46 +0400

    Ансамбли

---
+++ 7/majority_classifier.py
@@ -0,0 +1,198 @@
+import sys
+from scipy.special import comb
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.base import BaseEstimator
+from sklearn.base import ClassifierMixin
+from sklearn.preprocessing import LabelEncoder
+from sklearn.base import clone
+from sklearn.pipeline import _name_estimators
+import operator
+from sklearn import datasets
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier 
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import roc_curve
+from sklearn.metrics import auc
+from itertools import product
+from sklearn.model_selection import GridSearchCV
+import pandas as pd
+from sklearn.ensemble import BaggingClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.ensemble import AdaBoostClassifier
+import xgboost as xgb
+
+
+# Реализация взвешенного большинства
+np.argmax(np.bincount([0, 0, 1], weights=[0.2, 0.2, 0.6]))
+
+
+# Реализация взвешенного большинства на основе вероятностей
+ex = np.array([[0.9, 0.1],
+               [0.8, 0.2],
+               [0.4, 0.6]])
+
+p = np.average(ex, 
+               axis=0, 
+               weights=[0.2, 0.2, 0.6])
+print(p)
+print(np.argmax(p))
+
+
+
+import sklearn
+base_classes = (ClassifierMixin, BaseEstimator) if sklearn.__version__ >= "0.16" else (BaseEstimator, ClassifierMixin)
+
+
+class MajorityVoteClassifier(*base_classes):
+    """ Классификатор ансамблей с большинством голосов
+
+        Параметры
+        ----------
+        classifiers : массивоподобные, shape = [n_classifiers]
+        Разные классификаторы для ансамбля
+
+        vote : str, {'classlabel', 'probability'} (по умолчанию='classlabel')
+        Если 'classlabel', то прогноз основан на значении argmax
+        меток классов. В противном случае, если 'probability',
+        то для прогнозирования метки класса используется значение argmax суммы вероятностей
+        (рекомендуется для калиброванных классификаторов).
+
+        weights : в виде массива, shape = [n_classifiers], необязательно (по умолчанию=None)
+        Если указан список значений `int` или `float`, классификаторы
+        взвешиваются по важности; используются одинаковые веса, если `weights=None`.
+
+    """
+    def __init__(self, classifiers, vote='classlabel', weights=None):
+
+        self.classifiers = classifiers
+        self.named_classifiers = {key: value for key, value in _name_estimators(classifiers)}
+        self.vote = vote
+        self.weights = weights
+
+    def fit(self, X, y):
+        if self.vote not in ('probability', 'classlabel'):
+            raise ValueError(f"vote must be 'probability' or 'classlabel'"
+                             f"; got (vote={self.vote})")
+
+        if self.weights and len(self.weights) != len(self.classifiers):
+            raise ValueError(f'Number of classifiers and weights must be equal'
+                             f'; got {len(self.weights)} weights,'
+                             f' {len(self.classifiers)} classifiers')
+
+        # Use LabelEncoder to ensure class labels start with 0, which
+        # is important for np.argmax call in self.predict
+        self.lablenc_ = LabelEncoder()
+        self.lablenc_.fit(y)
+        self.classes_ = self.lablenc_.classes_
+        self.classifiers_ = []
+        for clf in self.classifiers:
+            fitted_clf = clone(clf).fit(X, self.lablenc_.transform(y))
+            self.classifiers_.append(fitted_clf)
+        return self
+
+    def predict(self, X):
+        if self.vote == 'probability':
+            maj_vote = np.argmax(self.predict_proba(X), axis=1)
+        else:  # 'classlabel' vote
+
+            #  Collect results from clf.predict calls
+            predictions = np.asarray([clf.predict(X)
+                                      for clf in self.classifiers_]).T
+
+            maj_vote = np.apply_along_axis(
+                                      lambda x:
+                                      np.argmax(np.bincount(x, weights=self.weights)),
+                                      axis=1,
+                                      arr=predictions)
+        maj_vote = self.lablenc_.inverse_transform(maj_vote)
+        return maj_vote
+
+    def predict_proba(self, X):
+        """ Предсказать вероятности классов для X.
+
+            Параметры
+            ----------
+            X : {массивоподобная, разреженная матрица}, shape = [n_examples, n_features]
+            Обучающие векторы, где n_examples - это количество примеров, а
+            n_features - количество объектов.
+
+            Возвращается
+            ----------
+            avg_proba : массивоподобный, shape = [n_примеров, n_классов]
+            Средневзвешенная вероятность для каждого класса в каждом примере.
+
+        """
+        probas = np.asarray([clf.predict_proba(X)
+                             for clf in self.classifiers_])
+        avg_proba = np.average(probas, axis=0, weights=self.weights)
+        return avg_proba
+
+    def get_params(self, deep=True):
+        """ Получение имен параметров классификатора для GridSearch"""
+        if not deep:
+            return super().get_params(deep=False)
+        else:
+            out = self.named_classifiers.copy()
+            for name, step in self.named_classifiers.items():
+                for key, value in step.get_params(deep=True).items():
+                    out[f'{name}__{key}'] = value
+            return out
+
+
+
+
+iris = datasets.load_iris()
+X, y = iris.data[50:, [1, 2]], iris.target[50:]
+le = LabelEncoder()
+y = le.fit_transform(y)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, 
+                        test_size=0.5, 
+                        random_state=1,
+                        stratify=y)
+
+
+clf1 = LogisticRegression(penalty='l2', 
+                          C=0.001,
+                          solver='lbfgs',
+                          random_state=1)
+
+clf2 = DecisionTreeClassifier(max_depth=1,
+                              criterion='entropy',
+                              random_state=0)
+
+clf3 = KNeighborsClassifier(n_neighbors=1,
+                            p=2,
+                            metric='minkowski')
+
+pipe1 = Pipeline([['sc', StandardScaler()],
+                  ['clf', clf1]])
+pipe3 = Pipeline([['sc', StandardScaler()],
+                  ['clf', clf3]])
+
+clf_labels = ['Logistic regression', 'Decision tree', 'KNN']
+    
+
+# Majority Rule (hard) Voting
+
+mv_clf = MajorityVoteClassifier(classifiers=[pipe1, clf2, pipe3])
+
+clf_labels += ['Majority voting']
+all_clf = [pipe1, clf2, pipe3, mv_clf]
+
+for clf, label in zip(all_clf, clf_labels):
+    scores = cross_val_score(estimator=clf,
+                             X=X_train,
+                             y=y_train,
+                             cv=10,
+                             scoring='roc_auc')
+    print(f'ROC AUC: {scores.mean():.2f} '
+          f'(+/- {scores.std():.2f}) [{label}]')
+    
+print(mv_clf.get_params())
